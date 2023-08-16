038e0150c68654c96a88da2475e898a6a55f3f6a
watt <wate@micous.com>
2023-08-16 12:17:17 +0800

    qr code

--- cmd/qrcode/go.mod
+++ cmd/qrcode/go.mod
@@ -1,3 +1,16 @@
-module qrcode
+module github.com/dxasu/tools/cmd/qrcode
 
 go 1.18
+
+require (
+	github.com/atotto/clipboard v0.1.4
+	github.com/dxasu/qrcode v1.0.0
+	github.com/spf13/cast v1.5.1
+	github.com/tuotoo/qrcode v0.0.0-20220425170535-52ccc2bebf5d
+)
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/maruel/rs v1.1.0 // indirect
+	rsc.io/qr v0.2.0 // indirect
+)
--- cmd/qrcode/main.go
+++ cmd/qrcode/main.go
@@ -1,18 +1,75 @@
 package main
 
 import (
-	"fmt"
+	"image/color"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
+	"github.com/atotto/clipboard"
 	"github.com/dxasu/qrcode"
+	"github.com/spf13/cast"
+	unQrCode "github.com/tuotoo/qrcode"
 )
 
 func main() {
-	if len(os.Args) == 0 {
-		fmt.Errorf("args empty")
+	var content string
+	if len(os.Args) >= 2 {
+		content = os.Args[1]
+	} else {
+		panic(`args empty, input http://xxxxx
+		 -c or "-cs 256" with copy data from clipboard
+		 -u xxx.png | http://xxxx.png with unpack from qrcode`)
+	}
+
+	length := 256
+	if content == "-c" || content == "-cs" && len(os.Args) >= 3 {
+		var err error
+		content, err = clipboard.ReadAll()
+		panicIf(err)
+
+		if content == "-cs" {
+			length = cast.ToInt(os.Args[2])
+		}
+	} else if content == "-u" && len(os.Args) >= 3 {
+		file := os.Args[2]
+		println(unpack(file))
 		return
 	}
 
-	url := os.Args[0]
-	q, _ := qrcode.New(url, qrcode.High)
+	err := qrcode.WriteColorFile(content, qrcode.Medium, length, color.Black, color.White, "qr.png")
+	panicIf(err)
+
+}
+
+func panicIf(err error) {
+	if err != nil {
+		println(err)
+		os.Exit(0)
+	}
+}
+
+func unpack(qrCodePath string) string {
+	var (
+		qc  *unQrCode.Matrix
+		err error
+	)
+
+	if strings.HasPrefix(qrCodePath, "http") {
+		_, err = url.Parse(qrCodePath)
+		panicIf(err)
+		resp, err := http.Get(qrCodePath)
+		panicIf(err)
+		defer resp.Body.Close()
+		qc, err = unQrCode.Decode(resp.Body)
+		panicIf(err)
+	} else {
+		file, err := os.Open(qrCodePath)
+		panicIf(err)
+		defer file.Close()
+		qc, err = unQrCode.Decode(file)
+		panicIf(err)
+	}
+	return qc.Content
 }
