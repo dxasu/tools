e32a1ec99d9922e296b9b006960bef2cbba96016
watt <watt@micous.com>
2025-07-16 16:37:27 +0800

    privority

--- cmd/cutpic/main.go
+++ cmd/cutpic/main.go
@@ -52,12 +52,12 @@ func calculateVariance(groupSums []float64) float64 {
 }
 
 // findBestCutLine 滑动窗口寻找最小方差切割线
-func findBestCutLine(img image.Image, endY, margin int) int {
+func findBestCutLine(img image.Image, endY, diffHeight int) int {
 	height := img.Bounds().Dy()
 	minVariance := math.MaxFloat64
 	bestCutY := endY
 
-	for delta := 0; delta <= margin; delta += windowStep {
+	for delta := 0; delta <= int(float64(diffHeight)*window); delta += windowStep {
 		currentCutY := endY + delta
 	MINUS:
 		if currentCutY < 0 || currentCutY > height {
@@ -83,12 +83,15 @@ func findBestCutLine(img image.Image, endY, margin int) int {
 }
 
 // splitImage 切割图片为高宽比2.5的子图（修正高宽比逻辑）
-func splitImage(img image.Image, margin int) []image.Image {
+func splitImage(img image.Image) []image.Image {
 	bounds := img.Bounds()
 	width := bounds.Dx()
 	height := bounds.Dy()
 	// subHeight := int(float64(width) * targetRatio)
 	divNum := int(math.Round(float64(height) / (float64(width) * targetRatio)))
+	if divNum == 0 {
+		panic(fmt.Sprintf("targetRatio is too large, must be smaller than %.2f, recommand to use -n flag", float64(height)/float64(width)))
+	}
 	subHeight := height / divNum
 	if windowStep*4 > subHeight {
 		panic(fmt.Sprintf("windowStep:%d too big, need be smaller than %d", windowStep, subHeight/4))
@@ -101,7 +104,7 @@ func splitImage(img image.Image, margin int) []image.Image {
 			endY = height // 最后一子图可能不足目标高度
 		} else {
 			// 动态调整切割线
-			endY = findBestCutLine(img, endY, margin)
+			endY = findBestCutLine(img, endY, subHeight)
 		}
 
 		// 切割并保存子图
@@ -123,23 +126,27 @@ func printImg(name string, img image.Image) {
 }
 
 var (
-	targetRatio = 2.0 // 2800/1260
-	window      = 300 // ±300像素， 大概宽度一半的大小
-	// 在分辨率为 ​​1260×2800的手机上，微信聊天记录截屏，空白间隔部分，大概40像素，这里取30
-	windowStep    = 30
+	targetRatio = 2.0  // 2800/1260
+	subNum      = 0    // 分割成多少个子图片
+	window      = 0.25 // ±1260*0.25像素， 默认窗口大小为 子图片高度*0.25
+	// 在分辨率为 ​​1260×2800的手机上，微信聊天记录截屏，空白间隔部分，大概40像素，这里取20
+	windowStep    = 20
 	limitVariance = 1.0 // 图片分割线，所在位置，像素值低于方差1.0，则视为通过。必须>=0
 	filepath      = ""
+	onlyMsg       = false // 当false时，不生成子图片,只输出信息
 )
 
 func main() {
-	flag.Float64Var(&targetRatio, "t", 2.0, "期望子图片的宽高比，近似值")
-	flag.IntVar(&window, "w", 300, "在切割线±像素范围内，寻找合适切割点")
-	flag.IntVar(&windowStep, "s", 30, "在分辨率为 ​​1260×2800的手机上，微信聊天记录截屏，空白间隔部分，大概40像素，默认取30")
+	flag.Float64Var(&targetRatio, "t", 2, "期望子图片的宽高比，近似值")
+	flag.IntVar(&subNum, "n", 0, "期望子图片的数量，默认值为0，无效值")
+	flag.Float64Var(&window, "w", 0.25, "在切割线±像素范围内，寻找合适切割点，默认窗口大小为 子图片*0.25")
+	flag.IntVar(&windowStep, "s", 20, "在分辨率为 ​​1260×2800的手机上，微信聊天记录截屏，空白间隔部分，大概40像素，默认取20")
 	flag.Float64Var(&limitVariance, "v", 1.0, "图片分割线所在位置，像素的方差值，低于limitVariance则视为合适的分割点")
+	flag.BoolVar(&onlyMsg, "o", false, "当true时，不生成子图片,只输出信息")
 
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s [options] <image_path>\n", os.Args[0])
-		fmt.Fprintln(os.Stderr, "Options:")
+		fmt.Fprintln(os.Stderr, "Options:（如果-n存在，则-t失效）")
 		flag.PrintDefaults() // 自动打印所有定义的参数及说明
 	}
 
@@ -151,7 +158,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(targetRatio, positionalArgs)
 	filepath := positionalArgs[0]
 
 	// 读取图片
@@ -166,6 +172,13 @@ func main() {
 		panic(err)
 	}
 
+	// 根据subNum 修正 targetRatio
+	if subNum != 0 {
+		targetRatio = float64(img.Bounds().Dy()) / float64(subNum) / float64(img.Bounds().Dx())
+		fmt.Println("targetRatio:", targetRatio)
+	}
+
+	fileDir := path.Dir(filepath)
 	// 获取path路径的文件名
 	fileName := path.Base(filepath)
 	printImg(fileName, img)
@@ -174,12 +187,21 @@ func main() {
 	fileName = fileName[:len(fileName)-len(ext)]
 
 	// 切割图片（目标高宽比，滑动窗口±像素）
-	subImages := splitImage(img, window)
+	subImages := splitImage(img)
 
 	for i, subImg := range subImages {
-		outFile, _ := os.Create(fmt.Sprintf("%s_%d.jpg", fileName, i))
+		subName := fmt.Sprintf("%s/%s_%d.jpg", fileName, fileName, i)
+		printImg(subName, subImg)
+		if onlyMsg {
+			continue
+		}
+		subName = fileDir + "/" + subName
+		err := os.MkdirAll(path.Dir(subName), 0755)
+		if err != nil {
+			panic(fmt.Errorf("创建目录失败: %v", err))
+		}
+		outFile, _ := os.Create(subName)
 		defer outFile.Close()
 		jpeg.Encode(outFile, subImg, nil)
-		printImg(outFile.Name(), subImg)
 	}
 }
